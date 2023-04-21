866d514e1b76f373c874345878875cb0fd864e50
RomanLeca <roman.leca@yahoo.com>
2023-04-21 08:00:45 +0300

    fix: optimized

--- tests/frontend/test_indexer_page.py
+++ tests/frontend/test_indexer_page.py
@@ -11,12 +11,12 @@ def indexer_search_page(page_mod):
     """
     page = page_mod
     page.goto(f"{BASE_URL}:{FE_PORT}")
-    yield page
+    yield SearchPage(page)
 
 
 def test_page_title_is_visible(indexer_search_page):
-    sp = SearchPage(indexer_search_page)
-    assert sp.page_title.is_visible()
+    page = indexer_search_page
+    assert page.page_title.is_visible()
 
 
 @pytest.mark.parametrize("field_value", [HASH_VALUE, FROM_ADDRESS, TO_ADDRESS])
@@ -24,7 +24,7 @@ def test_field_values_are_correct(indexer_search_page, field_value):
     """
     These tests will check various static values matches
     """
-    page = SearchPage(indexer_search_page)
+    page = indexer_search_page
     page.search_for(HASH_VALUE)
     page.RESULTS_CONTAINER.get_by_text(field_value)
     assert page.results.is_visible()
@@ -36,7 +36,7 @@ def test_frontend_and_api_are_in_sync(indexer_search_page, indexer_payload):
     The api payload can optionally be passed in as a dictionary to parametrize, thus avoiding loops in tests.
     This will keep every assertion independent, but also increase the overall number of reported tests.
     """
-    page = SearchPage(indexer_search_page)
+    page = indexer_search_page
     page.search_for(HASH_VALUE)
     for key, value in indexer_payload.items():
         page.RESULTS_CONTAINER.get_by_text(f"{key} - {value}").is_visible()
@@ -46,7 +46,7 @@ def test_wrong_hash_produces_no_results(indexer_search_page):
     """
     This is a negative test to check that results /div component doesn't display in DOM if there are no matches
     """
-    page = SearchPage(indexer_search_page)
+    page = indexer_search_page
     page.search_for(HASH_VALUE+"non_existing")
     assert not page.RESULTS_CONTAINER.is_visible()
 
